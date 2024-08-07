0ed63904156c952990d37d14cec701e9fac1d904
jackwhoami <jack.whoami@gmail.com>
2024-08-07 07:45:34 -0500

    Commit

--- app/Filament/Admin/Resources/MemberResource/Pages/ListMembers.php
+++ app/Filament/Admin/Resources/MemberResource/Pages/ListMembers.php
@@ -44,15 +44,15 @@ class ListMembers extends ListRecords
     public function getTabs(): array
     {
         return [
+          __('models/member.resource.table.tabs.requests') => Tab::make()
+            ->badge(Member::query()->where('membership_state', MembershipState::PENDING)->count())
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('membership_state', MembershipState::PENDING)),
           __('models/member.resource.table.tabs.visitors') => Tab::make()
             ->badge(Member::query()->where('type', MemberType::VISITOR)->count())
             ->modifyQueryUsing(fn (Builder $query) => $query->where('type', MemberType::VISITOR)),
           __('models/member.resource.table.tabs.members') => Tab::make()
             ->badge(Member::query()->where('type', MemberType::MEMBER)->count())
             ->modifyQueryUsing(fn (Builder $query) => $query->where('type', MemberType::MEMBER)),
-          __('models/member.resource.table.tabs.requests') => Tab::make()
-            ->badge(Member::query()->where('membership_state', MembershipState::PENDING)->count())
-            ->modifyQueryUsing(fn (Builder $query) => $query->where('membership_state', MembershipState::PENDING)),
         ];
     }
 }
