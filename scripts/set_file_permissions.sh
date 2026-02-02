959f9e746b856a7a8f83a2c73c0eadbbefd372ea
jackwhoami <jack.whoami@gmail.com>
2026-02-02 18:09:29 -0500

    Commit

---
+++ scripts/set_file_permissions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo chown -R $USER:www-data .
+
+sudo find . \( -path ./vendor -o -path ./node_modules -o -path ./.git -o -path ./deploy \) -prune -type f -o -exec chmod 644 {} \;
+sudo find . \( -path ./vendor -o -path ./node_modules -o -path ./.git -o -path ./deploy \) -prune -type d -o -exec chmod 755 {} \;
+
+sudo find ./storage -type f -exec chmod 644 {} \;
+sudo find ./storage -type d -exec chmod 775 {} \;
+
+sudo find ./bootstrap/cache/ -type f -exec chmod 644 {} \;
+sudo find ./bootstrap/cache/ -type d -exec chmod 775 {} \;
+
+sudo chmod -R 755 artisan
+sudo find . \( -path ./vendor -o -path ./node_modules -o -path ./.git -o -path ./deploy \) -prune -type f -name "*.sh" -o -exec chmod 755 {} \;
