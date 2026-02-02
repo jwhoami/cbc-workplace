33dd42d6316869c1d96c3bc4f60d1101c12c75fa
jackwhoami <jack.whoami@gmail.com>
2026-02-02 18:46:19 -0500

    Commit

--- scripts/set_file_permissions.sh
+++ scripts/set_file_permissions.sh
@@ -8,8 +8,8 @@ sudo find . \( -path ./vendor -o -path ./node_modules -o -path ./.git -o -path .
 sudo find ./storage -type f -exec chmod 644 {} \;
 sudo find ./storage -type d -exec chmod 775 {} \;
 
-sudo find ./bootstrap/cache/ -type f -exec chmod 644 {} \;
-sudo find ./bootstrap/cache/ -type d -exec chmod 775 {} \;
+sudo chmod 775 ./bootstrap/cache
+sudo find ./bootstrap/cache -type f -exec chmod 644 {} \;
 
 sudo chmod -R 755 artisan
 sudo find . \( -path ./vendor -o -path ./node_modules -o -path ./.git -o -path ./deploy \) -prune -type f -name "*.sh" -o -exec chmod 755 {} \;
