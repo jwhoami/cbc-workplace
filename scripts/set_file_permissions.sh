1eabccedb7c50007c92fda80258707bfff7599bb
jackwhoami <jack.whoami@gmail.com>
2026-02-02 19:29:11 -0500

    Commit

--- scripts/set_file_permissions.sh
+++ scripts/set_file_permissions.sh
@@ -2,14 +2,14 @@
 
 sudo chown -R $USER:www-data .
 
-sudo find . \( -path ./vendor -o -path ./node_modules -o -path ./.git -o -path ./deploy \) -prune -type f -o -exec chmod 644 {} \;
-sudo find . \( -path ./vendor -o -path ./node_modules -o -path ./.git -o -path ./deploy \) -prune -type d -o -exec chmod 755 {} \;
+sudo find . \( -path ./vendor -o -path ./node_modules -o -path ./.git -o -path ./deploy \) -prune -o -type f -exec chmod 644 {} \;
+sudo find . \( -path ./vendor -o -path ./node_modules -o -path ./.git -o -path ./deploy \) -prune -o -type d -exec chmod 755 {} \;
 
-sudo find ./storage -type f -exec chmod 644 {} \;
 sudo find ./storage -type d -exec chmod 775 {} \;
+sudo find ./storage -type f -exec chmod 664 {} \;
 
 sudo chmod 775 ./bootstrap/cache
-sudo find ./bootstrap/cache -type f -exec chmod 644 {} \;
+sudo find ./bootstrap/cache -type f -exec chmod 664 {} \;
 
-sudo chmod -R 755 artisan
-sudo find . \( -path ./vendor -o -path ./node_modules -o -path ./.git -o -path ./deploy \) -prune -type f -name "*.sh" -o -exec chmod 755 {} \;
+sudo chmod 755 artisan
+sudo find . \( -path ./vendor -o -path ./node_modules -o -path ./.git -o -path ./deploy \) -prune -o -type f -name "*.sh" -exec chmod 755 {} \;
